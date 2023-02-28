28c5b648c058bc979e20bc20af92167ae7eed634
chirikchekmen <chekmenev2004@mail.ru>
2023-02-28 18:46:41 +0100

    file fix

--- app.py
+++ app.py
@@ -1,27 +1,81 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, abort, send_from_directory, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getcwd, environ
+from os.path import isfile
+from os.path import join as join_path
+from json import load as json_load
 app = Flask(__name__)
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-@app.route('/')
+class FeedbackModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    feedback = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"FeedbackModel(id={self.name}, name={self.name}, email={self.email})"
+
+
+def getPublications():
+    with open("static/publications.json", "r") as publications:
+        return json_load(publications)
+
+
+@app.errorhandler(404)
+def code_404(error):
+    return render_template("404.html"), 404
+
+
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('index.html')
+    publications = getPublications()
+    return render_template("index.html", publications=publications)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template("projects.html")
 
 
-@app.route('/contacts')
+@app.route("/contacts", methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html')
+    if request.method == "GET":
+        return render_template("contacts.html")
+    data = request.get_json(force=True)
+    db.session.add(FeedbackModel(name=data["name"],
+                                 email=data["email"],
+                                 feedback=data["feedback"]))
+    db.session.commit()
+    return render_template("contacts.html")
 
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+@app.route("/publication/<int:publication_id>")
+def examples(publication_id):
+    if not isfile(join_path(getcwd(), "templates", "publications", f"{publication_id}.html")):
+        abort(404)
+    return render_template(f"/publications/{publication_id}.html")
+
+
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+@app.route('/serviceworker.js')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,81 +1,139 @@
-gsap.registerPlugin(ScrollTrigger);
-const duration = 1;
-const swup = new Swup();
+Prism.plugins.autoloader.languages_path = "/static/node_modules/prismjs/components/";
+gsap.registerPlugin(ScrollTrigger, CSSRulePlugin);
+const duration = 0.5;
+const animationStartPosition = "top 80%";
+const swup = new Swup({
+  plugins: [
+    new SwupScrollPlugin({
+      doScrollingRightAway: true,
+      offset: () => document.querySelector("nav.navbar").offsetHeight,
+    }),
+    new SwupSlideTheme({ reversed: false }),
+    new SwupPreloadPlugin(),
+  ],
+});
+
+window.onload = () => {
+  document.querySelector(".loader-wrapper").style.opacity = 0;
+  document.querySelector(".main-wrapper").style.opacity = 1;
+};
+
 const initialiseAllAnimations = () => {
   const onloadTimeline = gsap.timeline({
     scrollTrigger: {
       trigger: "section",
-      start: "top 90%",
+      start: animationStartPosition,
     },
   });
-
-  const cardsTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cards-wrapper",
-      start: "top 90%",
-    },
-  });
-  const footerTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: "section.footer",
-      start: "top 90%",
-    },
-  });
-  const cards = gsap.utils.toArray(".card");
-  cards.forEach((card) => {
-    gsap.from(card, {
+  if (document.querySelector(".cards-wrapper")) {
+    const cardsTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".cards-wrapper",
+        start: animationStartPosition,
+      },
+    });
+    cardsTimeline.from(".cards-wrapper", {
       x: 100,
       y: 100,
       opacity: 0,
+      duration: duration,
+    });
+  }
+  const cards = gsap.utils.toArray(".card");
+  cards.forEach((card) => {
+    gsap.from(card, {
+      opacity: 1,
       scrollTrigger: {
         trigger: card,
-        start: "top 90%",
+        start: animationStartPosition,
       },
     });
   });
-
-  onloadTimeline.from(".column-content", { x: -100, opacity: 0, duration: duration });
-  onloadTimeline.from("section.glassy .column img", {
-    x: 100,
-    opacity: 0,
-    duration: duration,
+  if (document.querySelector(".skill-bars")) {
+    document.querySelectorAll(".progress-line span").forEach((bar) => {
+      bar.style.width = bar.dataset.progress + "%";
+    });
+    const skillsBarTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".bar",
+        start: animationStartPosition,
+      },
+    });
+    skillsBarTimeline.to(".info span", {
+      opacity: 1,
+      duration: duration,
+    });
+    skillsBarTimeline.to(".progress-line", {
+      scaleX: 1,
+      duration: duration,
+    });
+    skillsBarTimeline.to(".progress-line span", {
+      scaleX: 1,
+      duration: duration,
+    });
+    skillsBarTimeline.to(
+      CSSRulePlugin.getRule(".progress-line span::after"),
+      {
+        opacity: 1,
+        duration: duration,
+      },
+      `-=${duration}`
+    );
+    skillsBarTimeline.to(
+      CSSRulePlugin.getRule(".progress-line span::before"),
+      {
+        opacity: 1,
+        duration: duration,
+      },
+      `-=${duration}`
+    );
+  }
+  const skills = gsap.utils.toArray(".card");
+  skills.forEach((skill) => {
+    gsap.from(skill, {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: skill,
+        start: animationStartPosition,
+      },
+    });
   });
+  if (document.querySelector("section .column img")) {
+    onloadTimeline.from(".column-content", { x: -100, opacity: 0, duration: duration });
+    onloadTimeline.from("section .column img", {
+      x: 100,
+      opacity: 0,
+      duration: duration,
+    });
+  }
   onloadTimeline.from(
     "main section:not(:first-child)",
     { opacity: 0, duration: 1 },
     `-=${duration}`
   );
-  cardsTimeline.from(".cards-wrapper", {
-    x: 100,
-    y: 100,
-    opacity: 0,
-    duration: duration,
-  });
-  footerTimeline.from("section.footer > .container", {
-    y: 50,
-    opacity: 0,
-    duration: duration,
-  });
 
   VanillaTilt.init(document.querySelectorAll("[data-tilt-element]"), {
     max: 10,
     speed: 600,
     scale: 1.05,
   });
+  document.querySelectorAll("code").forEach((block) => {
+    Prism.highlightElement(block);
+  });
 };
 initialiseAllAnimations();
-swup.on("contentReplaced", initialiseAllAnimations);
+swup.on("animationInStart", initialiseAllAnimations);
 
-const nav = document.querySelector("nav.navbar")
+// const nav = document.querySelector("nav.navbar");
 
-const navbarObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      nav.classList.toggle("glassy", entry.isIntersecting);
-    });
-  },
-  {
-    rootMargin: "0px",
-  }
-);
-navbarObserver.observe(document.querySelector("section"));
+// const navbarObserver = new IntersectionObserver(
+//   (entries, observer) => {
+//     entries.forEach((entry) => {
+//       nav.classList.toggle("glassy", entry.isIntersecting);
+//     });
+//   },
+//   {
+//     rootMargin: "0px",
+//   }
+// );
+// navbarObserver.observe(document.querySelector("section"));
